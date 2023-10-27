844d159258fa2e48a41a1465594c12fc106f34d6
autores-uk <autores.uk@proton.me>
2023-10-27 19:10:22 +0100

    GraalVM native-image test case

--- maven/native/src/main/java/uk/autores/integration/nativeapp/Main.java
+++ maven/native/src/main/java/uk/autores/integration/nativeapp/Main.java
@@ -1,6 +1,8 @@
 package uk.autores.integration.nativeapp;
 
 import uk.autores.GenerateByteArraysFromFiles;
+import uk.autores.GenerateMessagesFromProperties;
+import uk.autores.GenerateStringsFromText;
 import uk.autores.ResourceFiles;
 import uk.autores.cfg.Strategy;
 
@@ -11,8 +13,19 @@ import static uk.autores.cfg.Strategy.STRATEGY;
         handler = GenerateByteArraysFromFiles.class,
         config = @ResourceFiles.Cfg(key = STRATEGY, value = Strategy.INLINE)
 )
+@ResourceFiles(
+        value = "InlineString.txt",
+        handler = GenerateStringsFromText.class,
+        config = @ResourceFiles.Cfg(key = STRATEGY, value = Strategy.INLINE)
+)
+@ResourceFiles(
+        value = "InlineMessage.properties",
+        handler = GenerateMessagesFromProperties.class
+)
 public class Main {
     public static void main(String[] args) {
         System.out.writeBytes(InlineBytes.bytes());
+        System.out.print(InlineString.text());
+        System.out.println(InlineMessage.msg("Hello!"));
     }
 }
