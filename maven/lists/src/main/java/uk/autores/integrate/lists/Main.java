83d2e694a0c8a440b3cadad863ba88a50af97010
autores-uk <autores.uk@proton.me>
2025-04-03 17:24:35 +0100

    Test ListFormat support

---
+++ maven/lists/src/main/java/uk/autores/integrate/lists/Main.java
@@ -0,0 +1,18 @@
+package uk.autores.integrate.lists;
+
+import uk.autores.Messages;
+
+import java.util.List;
+import java.util.Locale;
+
+@Messages("jdk23.properties")
+public class Main {
+    public static void main(String[] args) {
+        var l = Locale.getDefault();
+        var list = List.of("foo", "bar", "baz");
+
+        System.out.println(Jdk23.standard(l, list));
+        System.out.println(Jdk23.unit(l, list));
+        System.out.println(Jdk23.or(l, list));
+    }
+}
