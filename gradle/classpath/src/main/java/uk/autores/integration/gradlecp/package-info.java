74c6bb516dbfce01fa7b14c734c053d702daac94
autores-uk <autores.uk@proton.me>
2025-04-29 15:23:46 +0100

    Fixed visibility

--- gradle/classpath/src/main/java/uk/autores/integration/gradlecp/package-info.java
+++ gradle/classpath/src/main/java/uk/autores/integration/gradlecp/package-info.java
@@ -1,4 +1,5 @@
-@Texts(value = "Foo.txt", isPublic = true)
+@Texts(value = "Foo.txt", visibility = Visibility.PUBLIC)
 package uk.autores.integration.gradlecp;
 
 import uk.autores.Texts;
+import uk.autores.Visibility;
