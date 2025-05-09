8df46d2e255c60d6e92a8782175241ae220a8e8d
autores-uk <autores.uk@proton.me>
2025-05-09 12:52:33 +0100

    Upgraded JUnit lib

--- gradle/module/build.gradle.kts
+++ gradle/module/build.gradle.kts
@@ -20,9 +20,11 @@ var processing = "uk.autores:processing:" + libver
 dependencies {
     compileOnly(files("src/main/resources"))
     compileOnly(annotations)
+
     annotationProcessor(processing)
+
     testImplementation(annotations)
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
